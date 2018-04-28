9e3b7c8392938c17bb28ad5d06efa69d77384cb6
maksymmoskvychev <maksym.moskvychev@emesa.nl>
2018-04-28 20:37:16 +0200

    Refactored dependencies on AWS provider variables

--- variables.tf
+++ variables.tf
@@ -3,7 +3,4 @@ variable "certificate_arn" {}
 variable "document_root" {}
 variable "enabled" {}
 variable "route53_zone_id" {}
-variable "aws_region" {}
-variable "access_key" {}
-variable "secret_key" {}
 variable "cache_ttl" {}
