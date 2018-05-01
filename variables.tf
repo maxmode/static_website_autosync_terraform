0039c38bc7c0955714f519ebdf32225d97d58072
maksymmoskvychev <maksym.moskvychev@emesa.nl>
2018-05-01 20:13:25 +0200

    Added output variables

--- variables.tf
+++ variables.tf
@@ -4,3 +4,9 @@ variable "document_root" {}
 variable "enabled" {}
 variable "route53_zone_id" {}
 variable "cache_ttl" {}
+output "bucket" {
+  value = "${aws_s3_bucket.s3_website.bucket}"
+}
+output "cloudfront_domain" {
+  value = "${aws_cloudfront_distribution.s3_distribution.domain_name}"
+}
